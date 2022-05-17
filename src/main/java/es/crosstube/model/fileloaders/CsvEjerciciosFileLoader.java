3f6d20f04b8a97ffae0eddb5ca5a8851ec134ede
VictorMellado <victormm122003@gmail.com>
2022-05-17 21:43:08 +0200

    [ADD] Loaders CSVs

---
+++ src/main/java/es/crosstube/model/fileloaders/CsvEjerciciosFileLoader.java
@@ -0,0 +1,21 @@
+package es.crosstube.model.fileloaders;
+
+import es.crosstube.model.dto.Ejercicio;
+import es.crosstube.model.dto.NivelEsfuerzo;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class CsvEjerciciosFileLoader {
+    private static final String PATH_FILE = "ejercicios.csv";
+    private File file;
+
+    private ArrayList<Ejercicio> getEjercicios() {
+        return null;
+    }
+
+    public void insertarEjercicios() {
+        ArrayList<Ejercicio> niveles = getEjercicios();
+
+    }
+}
